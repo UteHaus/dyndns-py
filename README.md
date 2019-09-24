3e19102c4dfbfee89fd51e5707958f37ab50dcaf
Johannes V <j.v@posteo.de>
2019-09-24 20:21:31 +0200

    2

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # dyndns-py
+
+### Installa Dependencies:
+
+```sh
+    pip install fritzconnection
+```
+
+and fritzconnection requires the python modules `lxml` and `requests`:
+
+```sh
+    pip install lxml requests
+```
--- update-ip.py
+++ update-ip.py
@@ -1,12 +1,30 @@
 import socket
 import urllib.request
+import fritzconnection as fc
+from fritzconnection import FritzStatus
+import sys
 
-print(socket.gethostbyname(socket.gethostname()))
-print(socket.gethostbyname("wobco.de"))
+fritzPassword = 'F2!L16=f&f'
+domain = 'wobco.de'
+print(str(sys.argv))
+fs = FritzStatus(password=fritzPassword)
+if (fs.is_connected):
+    print('FritzBox is connected')
+    domainIp = socket.gethostbyname(domain)
+    fritzboxIp = fs.external_ip
 
-# get my public ip
-external_ip = urllib.request.urlopen('https://ident.me').read().decode('utf8')
-print(external_ip)
+    if (domainIp == fritzboxIp):
+        print('Domain and FritzBox Ips ara same')
+        print('domain ip: ' + domainIp)
+        print('fritzbox ip' + fritzboxIp)
+    else:
+        updateNetcup(fritzboxIp)
 
 
-# get infos from fritzbox https://pypi.org/project/fritzconnection/
+def checkip(fs, ):
+    print('Ip Check for')
+    print('my ip ')
+
+
+def updateNetcup(ip):
+    print('update netcup ip with:' + ip)
